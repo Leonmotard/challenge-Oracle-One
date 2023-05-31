1158134d9da9b8f859412a55a7f12aa00597aafa
Leandro Silva <leandro.jj.silva@gmail.com>
2023-05-30 22:25:41 -0300

    Realización de función encriptar, validación de entrada y presentación de mensaje.

--- functions.js
+++ functions.js
@@ -1,16 +1,51 @@
 
+var botonEncriptar = document.getElementById("botonEncriptar");
+
+const textArea = document.getElementById("textoIngresado");
+
+const rectangulo = document.getElementById("rectangulo");
+
+const mensajeMostrar = document.querySelector(".mensajeEncriptado");
+
+const mensajeOcultar = document.querySelector(".mensajeVacio");
+/*
+const mensaje = document.querySelector("#mensaje");
+
+const parrafo = document.querySelector("#parrafoMensaje")
+*/
+const copia = document.querySelector(".copiar");
+copia.style.display = "none";
+mensajeMostrar.style.display = "none";
+
+botonEncriptar.onclick = codificarString;
 
 function codificarString(){
 
-	var frase = document.getElementById("textoIngresado").value;
-	var palabraEncriptada = "";
-	for(let i = 0; i < frase.length; i++){
+	var frase = textArea.value;
 
-		palabraEncriptada += reemplazarVocales(frase[i]);
+	if(!validarTexto(frase)){
 
+		var palabraEncriptada = "";
+		for(let i = 0; i < frase.length; i++){
+
+			palabraEncriptada += reemplazarVocales(frase[i]);
+
+		} 
+		/*
+		mensaje.textContent = palabraEncriptada;
+		mensaje.classList.remove('mensajeHeader');
+		*/
+		mensajeOcultar.style.display = "none";
+		mensajeMostrar.style.display = "block";
+		mensajeMostrar.textContent = palabraEncriptada;
+		rectangulo.style.backgroundImage= "none";
+		//textArea.value="";
+		copia.style.display = "block";
 	}
 
+	/*
 	console.log(palabraEncriptada);
+	*/
 }
 
 function reemplazarVocales(letra){
@@ -33,6 +68,25 @@ function reemplazarVocales(letra){
 		default:
 			return letra;
 	}
+}
+
+function decidificarString(texto){
+
+
+}
+
+/*
+/\b[a-z]+\b/g  reconoce todas las palabras incluyendo espacios y saltos de linea, pero no signos de puntuación.
+*/
+function validarTexto(texto){
+
+	let validador = texto.match(/\b[a-z]+\b/g);
+
+	if(!validador || validador === 0) {
+        alert("Solo son permitidas letras minúsculas y sin acentos")
+        location.reload();
+        return true;
+    }
 }
 
 
