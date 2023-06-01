f99ddad91bf674d1535865642d59a49a7a0a0d21
Leandro Silva <leandro.jj.silva@gmail.com>
2023-06-01 20:15:16 -0300

    Creación de la función desencriptar y mostrar en pantalla el mensaje decodificado, función de botón copiar, se agregaron comentarios al código.

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+// Declaración de variables y constantes.
 
 var botonEncriptar = document.getElementById("botonEncriptar");
 
@@ -8,22 +9,33 @@ const rectangulo = document.getElementById("rectangulo");
 const mensajeMostrar = document.querySelector(".mensajeEncriptado");
 
 const mensajeOcultar = document.querySelector(".mensajeVacio");
-/*
-const mensaje = document.querySelector("#mensaje");
 
-const parrafo = document.querySelector("#parrafoMensaje")
-*/
 const copia = document.querySelector(".copiar");
+
+
+/*Se ocultan el botón de copia y el parrafo que mostrará el mensaje una vez encriptado
+o desencriptado
+*/
 copia.style.display = "none";
 mensajeMostrar.style.display = "none";
 
+//Evento click del botón para encriptar el texto.
 botonEncriptar.onclick = codificarString;
 
+
+/*
+función que dispara la encriptación del texto, la misma recorre el string ingresado mediante un ciclo for 
+para luego llamar a la función que se encargará de reemplazar las vocales. Una vez encriptada la palabra modificará
+los elementos del DOM para ocultar la imágen del muñeco en la sección de salida y habilitará el parrafo con el texto ya encriptado
+y el botón para copiar el texto antes mencionado. 
+*/
 function codificarString(){
 
 	var frase = textArea.value;
 
 	if(!validarTexto(frase)){
+
+		
 
 		var palabraEncriptada = "";
 		for(let i = 0; i < frase.length; i++){
@@ -31,23 +43,27 @@ function codificarString(){
 			palabraEncriptada += reemplazarVocales(frase[i]);
 
 		} 
-		/*
-		mensaje.textContent = palabraEncriptada;
-		mensaje.classList.remove('mensajeHeader');
-		*/
+
 		mensajeOcultar.style.display = "none";
 		mensajeMostrar.style.display = "block";
 		mensajeMostrar.textContent = palabraEncriptada;
 		rectangulo.style.backgroundImage= "none";
-		//textArea.value="";
 		copia.style.display = "block";
 	}
 
-	/*
-	console.log(palabraEncriptada);
-	*/
+
 }
 
+/*
+Esta función reemplaza una letra siempre y cuando sea una vocal, por la clave de encriptación solicitada.
+Laves de encriptacion
+`La letra "e" es convertida para "enter"`
+`La letra "i" es convertida para "imes"`
+`La letra "a" es convertida para "ai"`
+`La letra "o" es convertida para "ober"`
+`La letra "u" es convertida para "ufat"`
+
+*/
 function reemplazarVocales(letra){
 
 	switch(letra){
@@ -70,12 +86,54 @@ function reemplazarVocales(letra){
 	}
 }
 
-function decidificarString(texto){
 
+/*
+Función que desencriptará un mensaje, la misma transforma el string ingresado en un array para hacer uso del metodo splice
+el cual se encargará de reemplazar la llave de encriptación por la vocal correspondiente para formar las palabras del mensaje.
+Una vez realizada la transformación y haciendo uso de los elementos del DOM mostrará en pantalla un parrafo con el mensaje 
+y el botón de copiar. 
+*/
+function decodificarString(){
+	var texto = textArea.value;
 
+	if(!validarTexto(texto)){
+		let arreglo = Array.from(texto);
+
+		for (let i = 0; i <= arreglo.length - 1; i++) {
+			
+			switch(arreglo[i]){
+			case "a":
+				arreglo.splice(i+1,1);
+				break;
+			case "e":
+				arreglo.splice(i+1, 4);
+				break;
+			case "i":
+				arreglo.splice(i+1, 3);
+				break;
+			case "o":
+				arreglo.splice(i+1, 3);
+				break;
+			case "u":
+				arreglo.splice(i+1, 3);
+				break;
+			}
+		}
+
+			texto = arreglo.join('');
+
+			mensajeOcultar.style.display = "none";
+			mensajeMostrar.style.display = "block";
+			mensajeMostrar.textContent = texto;
+			rectangulo.style.backgroundImage= "none";
+			copia.style.display = "block";
+	}
 }
 
 /*
+Función que hace uso de una expresión regular para comprobar que el texto ingresado no posea caracteres especiales,
+ni signos de puntuación. Devolviendo true en caso de poseer alguno de ellos junto a una ventana emergente avisando al usuario
+que ha ingresado texto no válido.
 /\b[a-z]+\b/g  reconoce todas las palabras incluyendo espacios y saltos de linea, pero no signos de puntuación.
 */
 function validarTexto(texto){
@@ -89,4 +147,15 @@ function validarTexto(texto){
     }
 }
 
+
+/*
+función que copia el texto del mensaje de salida(Encriptado o desencriptado, dependiendo de la acción realizada
+con anterioridad por el usuario).
+*/
+function copiar(){
+
+    navigator.clipboard.writeText(mensajeMostrar.innerHTML);
+    mensajeMostrar.innerHTML = "";
+    alert("Texto Copiado")
+}
 
